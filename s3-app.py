10eef3ba46e14cd97f0b9c023372c8ee74cb0dba
nagaraj-nk <nnagarajece31@gmail.com>
2022-09-25 13:13:02 +0530

    initial commit: aws-python-developer

---
+++ s3-app.py
@@ -0,0 +1,11 @@
+import boto3
+
+# Let's use Amazon S3
+s3 = boto3.resource('s3')
+# Print out bucket names
+for bucket in s3.buckets.all():
+    print(bucket.name)
+
+# Upload a new file
+data = open('test.jpg', 'rb')
+s3.Bucket('nagaraj-bucket-files').put_object(Key='test.jpg', Body=data)
